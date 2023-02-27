cbcca60f12dc3506e74f5a2ee83121e24d1b443f
Nikolay Pivnik <nikolaypivnik@gmail.com>
2023-02-27 22:03:16 +0300

    Change fonts location

--- Modules/Resources/Resources/Font/FontType.swift
+++ Modules/Resources/Resources/Font/FontType.swift
@@ -10,32 +10,3 @@ public enum FontType: String {
     case extraBold
     case system
 }
-
-// MARK: - Fonts used within the app
-
-//extension R {
-//    static let ptroot = "PTRootUI"
-//
-//    static func font(
-//        of name: String = R.ptroot,
-//        type: FontType = .regular,
-//        size: CGFloat
-//    ) -> UIFont {
-//        getFont(baseFontName: name, type: type, size: size)
-//    }
-//}
-
-// MARK: - Convenience methods
-
-//extension R {
-//    // swiftlint:disable force_unwrapping
-//    static func getFont(
-//        baseFontName: String,
-//        type: FontType,
-//        size: CGFloat
-//    ) -> UIFont {
-//        UIFont(name: "\(baseFontName)-\(type.rawValue)", size: size)!
-//    }
-//
-//    // swiftlint:enable force_unwrapping
-//}
--- Setup.command
+++ Setup.command
@@ -9,7 +9,6 @@ xcodegen generate
 sh PodInstall.command
 
 #resorces
-cd "Modules/Resources/Resources"
 swiftgen config lint
 swiftgen 
 
